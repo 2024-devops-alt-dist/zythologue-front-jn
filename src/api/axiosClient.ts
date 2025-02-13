40b8aaaf8dd7d7a868468e8187903dfe56f1e526
jimni6 <nijimmy@outlook.fr>
2025-02-13 12:35:51 +0100

    ci: new path for api

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000', // Update with your backend URL
+    baseURL: 'https://zythologue-api-jn.onrender.com', // Update with your backend URL
     headers: {
         'Content-Type': 'application/json',
     },
